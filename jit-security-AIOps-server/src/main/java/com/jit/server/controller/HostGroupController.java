2a37e456b3781f9598de33632d2c2501ad91ae96
苗增新 <mzxdyx123@163.com>
2020-06-29 16:28:54 +0800

    Project: jit-security-AIOps-server Author: zengxin_miao Date: 2020.06.29 Change: add HostGroupController

---
+++ jit-security-AIOps-server/src/main/java/com/jit/server/controller/HostGroupController.java
@@ -0,0 +1,54 @@
+package com.jit.server.controller;
+
+import com.jit.server.exception.ExceptionEnum;
+import com.jit.server.service.ZabbixAuthService;
+import com.jit.server.util.Result;
+import com.jit.zabbix.client.dto.ZabbixHostGroupDTO;
+import com.jit.zabbix.client.request.ZabbixGetHostGroupParams;
+import com.jit.zabbix.client.service.ZabbixHostGroupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author zengxin_miao
+ * @version 1.0
+ * @date 2020.06.29
+ */
+
+@RestController
+@RequestMapping("/hostGroup")
+public class HostGroupController {
+
+    @Autowired
+    private ZabbixHostGroupService zabbixHostGroupService;
+
+    @Autowired
+    private ZabbixAuthService zabbixAuthService;
+
+
+    /**
+     * get zabbix hostGroup
+     *
+     * @return
+     */
+    @ResponseBody
+    @PostMapping(value = "/getZabbixHostGroup")
+    public Result getZabbixHostGroup() {
+        try {
+            String auth = zabbixAuthService.getAuth();
+            ZabbixGetHostGroupParams params = new ZabbixGetHostGroupParams();
+            params.setOutput("extend");
+            List<ZabbixHostGroupDTO> zabbixHostGroupDTOList = zabbixHostGroupService.get(params, auth);
+            return Result.SUCCESS(zabbixHostGroupDTOList);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.ERROR(ExceptionEnum.QUERY_DATA_EXCEPTION);
+        }
+    }
+
+}
