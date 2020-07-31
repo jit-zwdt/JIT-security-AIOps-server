36862eec0882c19b9faec2decc156134f6ca2731
苗增新 <mzxdyx123@163.com>
2020-07-31 13:54:09 +0800

    Project: jit-security-AIOps-server/base Author: zengxin_miao Date: 2020.07.30 Change: add HomePageController

--- jit-security-AIOps-server/src/main/java/com/jit/server/controller/HomePageController.java
+++ jit-security-AIOps-server/src/main/java/com/jit/server/controller/HomePageController.java
@@ -11,10 +11,7 @@ import com.jit.zabbix.client.model.problem.ProblemSeverity;
 import com.jit.zabbix.client.request.ZabbixGetProblemParams;
 import com.jit.zabbix.client.service.ZabbixProblemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -80,8 +77,8 @@ public class HomePageController {
                 }
                 if (!hostIds.isEmpty()) {// the last param
                     jsonObject = new JSONObject();
-                    jsonObject.put("type", types.get(typeIds.indexOf(typeId) - 1));
-                    jsonObject.put("typeId", typeIds.get(typeIds.indexOf(typeId) - 1));
+                    jsonObject.put("type", type);
+                    jsonObject.put("typeId", typeId);
                     jsonObject.put("hostIds", hostIds);
                     jsonObject.put("hostCount", hostIds.size());
                     jsonObject.put("problemsSeverityCount", countProblemsGroupBySeverity(hostIds));
@@ -95,6 +92,13 @@ public class HomePageController {
         }
     }
 
+    /**
+     * count problem group by severity
+     *
+     * @param hostIds
+     * @return
+     * @throws Exception
+     */
     private Map<String, Integer> countProblemsGroupBySeverity(List<String> hostIds) throws Exception {
         Map<String, Integer> res = new HashMap<>();
         res.put("0", 0);
@@ -148,5 +152,60 @@ public class HomePageController {
             return res;
         }
     }
+
+    @ResponseBody
+    @PostMapping(value = "/getTimeTop5ItemInfo/{item}")
+    public Result getTimeTop5ItemInfo(@PathVariable String item) {
+        try {
+            List<Object> objectList = homePageService.getHostByType();
+            JSONArray jsonArray = new JSONArray();
+            if (objectList != null && !objectList.isEmpty()) {
+                String hostId = "";
+                String type = "";
+                String typeId = "";
+                List<String> types = new ArrayList<>();
+                List<String> typeIds = new ArrayList<>();
+                List<String> hostIds = null;
+                JSONObject jsonObject;
+                for (int i = 0, len = objectList.size(); i < len; i++) {
+                    Object[] objs = (Object[]) objectList.get(i);
+                    hostId = objs[0].toString();
+                    type = objs[1].toString();
+                    typeId = objs[2].toString();
+                    if (typeIds.contains(typeId)) {
+                        hostIds.add(hostId);
+                    } else {
+                        types.add(type);
+                        typeIds.add(typeId);
+                        if (i > 0) {//exclude hostIds null
+                            jsonObject = new JSONObject(5);
+                            jsonObject.put("type", types.get(typeIds.indexOf(typeId) - 1));
+                            jsonObject.put("typeId", typeIds.get(typeIds.indexOf(typeId) - 1));
+                            jsonObject.put("hostIds", hostIds);
+                            jsonObject.put("hostCount", hostIds.size());
+                            jsonObject.put("problemsSeverityCount", countProblemsGroupBySeverity(hostIds));
+                            jsonArray.add(jsonObject);
+                        }
+                        hostIds = new ArrayList<>();
+                        hostIds.add(hostId);
+                    }
+                }
+                if (!hostIds.isEmpty()) {// the last param
+                    jsonObject = new JSONObject();
+                    jsonObject.put("type", types.get(typeIds.indexOf(typeId) - 1));
+                    jsonObject.put("typeId", typeIds.get(typeIds.indexOf(typeId) - 1));
+                    jsonObject.put("hostIds", hostIds);
+                    jsonObject.put("hostCount", hostIds.size());
+                    jsonObject.put("problemsSeverityCount", countProblemsGroupBySeverity(hostIds));
+                    jsonArray.add(jsonObject);
+                }
+            }
+            return Result.SUCCESS(jsonArray);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.ERROR(ExceptionEnum.QUERY_DATA_EXCEPTION);
+        }
+    }
+
 
 }
