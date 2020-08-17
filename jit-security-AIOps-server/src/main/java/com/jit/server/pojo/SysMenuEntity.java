f2aba19edc074342598e08d17754cf5491699535
苗增新 <mzxdyx123@163.com>
2020-08-17 15:24:08 +0800

    Project: jit-security-AIOps-server Author: zengxin_miao Date: 2020.08.17 Change: table field update

--- jit-security-AIOps-server/src/main/java/com/jit/server/pojo/SysMenuEntity.java
+++ jit-security-AIOps-server/src/main/java/com/jit/server/pojo/SysMenuEntity.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
-import java.sql.Timestamp;
 
 /**
  * @Description: table sys_user_role entity
@@ -15,37 +14,117 @@ import java.sql.Timestamp;
 @Entity
 @Table(name = "sys_menu")
 public class SysMenuEntity {
+
+    /**
+     * 主键
+     */
     @Id
     @Column(length = 32, nullable = false)
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid")
     private String id;
 
-    @Column(name = "parent_id", nullable = false)
+
+    /**
+     * 父菜单ID，一级菜单为0
+     */
+    @Column(name = "parent_id")
     private String parentId;
 
+    /**
+     * 菜单名称
+     */
     @Column(name = "name")
     private String name;
 
+    /**
+     * 菜单URL
+     */
     @Column(name = "url")
     private String url;
 
+    /**
+     * 授权(多个用逗号分隔，如：user:list,user:create)
+     */
     @Column(name = "perms")
     private String perms;
 
-    @Column(name = "type")
-    private int type;
+    /**
+     * 权限策略 0: 显示；1：禁用
+     */
+    @Column(name = "perms_type")
+    private int permsType;
 
+    /**
+     * 组件
+     */
+    @Column(name = "component")
+    private String component;
+
+    /**
+     * 组件名称
+     */
+    @Column(name = "component_name")
+    private String componentName;
+
+    /**
+     * 类型   0：一级菜单   1：子菜单   2：按钮
+     */
+    @Column(name = "menu_type")
+    private int menuType;
+
+    /**
+     * 菜单图标
+     */
     @Column(name = "icon")
     private String icon;
 
+    /**
+     * 排序
+     */
     @Column(name = "order_num")
     private int orderNum;
 
-    @Column(name = "gmt_create")
-    private Timestamp gmtCreate;
+    /**
+     * 是否路由菜单: 0:不是  1:是（默认值1）
+     */
+    @Column(name = "is_route")
+    private int isRoute;
 
+    /**
+     * 是否隐藏路由: 0：否；1：是
+     */
+    @Column(name = "hidden_route")
+    private int hiddenRoute;
+
+    /**
+     * 创建时间
+     */
+    @Column(name = "gmt_create")
+    private java.sql.Timestamp gmtCreate;
+
+    /**
+     * 修改时间
+     */
     @Column(name = "gmt_modified")
-    private Timestamp gmtModified;
+    private java.sql.Timestamp gmtModified;
+
+    /**
+     * 创建者
+     */
+    @Column(name = "create_by")
+    private String createBy;
+
+    /**
+     * 更新者
+     */
+    @Column(name = "update_by")
+    private String updateBy;
+
+    /**
+     * 是否删除 1:表示删除;0:表示未删除
+     */
+    @Column(name = "is_deleted")
+    private int isDeleted;
 
 }
