46a894a775c372256af948a4ddf12da678dba6bd
jian_liu_ggzw <1115218686@qq.com>
2020-09-03 15:55:06 +0800

    更改内容：修改 更改人：jian_liu 更改时间：2020年9月3日15点55分

--- jit-security-AIOps-server/src/main/java/com/jit/server/repository/SysMenuRepo.java
+++ jit-security-AIOps-server/src/main/java/com/jit/server/repository/SysMenuRepo.java
@@ -1,12 +1,15 @@
 package com.jit.server.repository;
 
 import com.jit.server.pojo.SysMenuEntity;
+import com.jit.server.pojo.SysRoleMenuEntity;
+import com.jit.server.pojo.SysUserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @Description: SysMenuRepo
@@ -23,4 +26,9 @@ public interface SysMenuRepo extends JpaRepository<SysMenuEntity, String>, JpaSp
      */
     @Query("SELECT u.id,u.path,u.component,u.redirect,u.name,u.title,u.icon,u.isRoute,u.isShow,u.orderNum,u.sid FROM SysMenuEntity u WHERE  u.isDeleted = 0 and u.parentId = ?1 order by u.orderNum")
     List<Object> getMenus(String parentId);
+
+    @Query("SELECT srm FROM SysUserRoleEntity ur, SysRoleEntity sr, SysRoleMenuEntity srm WHERE sr.id = ur.roleId and sr.isDeleted = 0 and sr.id = srm.roleId and srm.isDeleted = 0 and ur.userId = ?1 and ur.isDeleted = 0")
+    List<SysRoleMenuEntity> findSysRoleMenuEntityList(String id);
+
+    Optional<SysMenuEntity> findByIdAndIsDeleted(String id, int isDeleted);
 }
