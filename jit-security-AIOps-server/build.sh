d21301b0a9236d5a36f5cf6c831cf275711d7ea7
frank.ren <junming_ren@jit.com.cn>
2021-01-14 16:05:14 +0800

    修改配置文件使用环境变量fix bug

--- jit-security-AIOps-server/build.sh
+++ jit-security-AIOps-server/build.sh
@@ -22,7 +22,7 @@ fi
 docker build -t $IMAGENAME:$VERSION $project
 
 # docker run -e TZ="Asia/Shanghai" -p 8081:8080 -idt --name jit-aiops-server --network zabbix_net  --ip=172.19.0.6  -v /var/log/tomcat:/usr/local/tomcat/logs  -e ZABBIX_API_URL=  $IMAGENAME:$VERSION
-docker run -e TZ="Asia/Shanghai" -p 8081:8080 -idt --name jit-aiops-server --network zabbix_net  --ip=172.19.0.6  -v /var/log/tomcat:/usr/local/tomcat/logs  -e ZABBIX_API_URL=172.19.0.5 -e ZABBIX_API_PORT=8080 -e ZABBIX_USERNAME=Admin -e ZABBIX_PASSWORD=zabbix -e DB_HOST=172.17.0.1 -e DB_PORT=3306 -e DB_NAME=jit-security-aiops-server -e DB_USER=root -e DB_PASSWORD=root  $IMAGENAME:$VERSION
+docker run -e TZ="Asia/Shanghai" -p 8081:8080 -idt --name jit-aiops-server --network zabbix_net  --ip=172.19.0.6  -v /var/log/tomcat:/usr/local/tomcat/logs  -e ZABBIX_APIURL=172.19.0.5 -e ZABBIX_APIPORT=8080 -e ZABBIXUSERNAME=Admin -e ZABBIXPASSWORD=zabbix -e DB_HOST=172.17.0.1 -e DB_PORT=3306 -e DB_NAME=jit-security-aiops-server -e DB_USER=root -e DB_PASSWORD=root  $IMAGENAME:$VERSION
 
 
 # docker tag $IMAGENAME:$VERSION $HARBORURI/$HARBORREPONAME/$IMAGENAME:$VERSION
